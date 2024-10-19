5a5f35c995726c2dcc79d7f702c4927362e86c7e
X58okYW8B54e <113420721+YQh9Mh3181jzEJ5@users.noreply.github.com>
2024-10-19 23:42:17 +0900

    feat: add card with shadcn

--- components/blog/BlogItem.tsx
+++ components/blog/BlogItem.tsx
@@ -3,6 +3,7 @@ import { BlogPost } from "@/app/types";
 import { format } from "date-fns";
 import Image from "next/image";
 import Link from "next/link";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
 
 interface BlogItemProps {
   blogPost: BlogPost;
@@ -15,50 +16,55 @@ const BlogItem = ({ blogPost, isRanking }: BlogItemProps) => {
   const formattedDate = format(new Date(createdAt), "yyyy/MM/dd");
 
   return (
-    <article className="border rounded group">
-      <Link href={`/blogs/${id}`}>
-        <div className="aspect-video relative overflow-hidden">
-          <Image
-            src={image.url}
-            width={768}
-            height={432}
-            alt={title}
-            className="object-cover transition-transform duration-100 ease-in-out group-hover:scale-105"
-            priority={false}
-          />
+    <article>
+      <Card className="group overflow-hidden">
+        <Link href={`/blogs/${id}`} className="block h-full">
+          <CardHeader className="p-0">
+            <div className="aspect-video relative overflow-hidden">
+              <Image
+                src={image.url}
+                width={768}
+                height={432}
+                alt={title}
+                className="object-cover transition-transform duration-100 ease-in-out group-hover:scale-105"
+                priority={false}
+              />
 
-          <div
-            className="absolute top-0 left-0 text-xs text-white py-1.5 px-4"
-            style={{ backgroundColor: categoryColor }}
-          >
-            {category.name}
-          </div>
+              <div
+                className="absolute top-0 left-0 text-xs text-white py-1.5 px-4"
+                style={{ backgroundColor: categoryColor }}
+              >
+                {category.name}
+              </div>
 
-          {isRanking && ranking && (
-            <div className="absolute top-0 right-0 bg-white py-2 px-3 font-bold">
-              {ranking}
+              {isRanking && ranking && (
+                <div className="absolute top-0 right-0 bg-white py-2 px-3 font-bold">
+                  {ranking}
+                </div>
+              )}
             </div>
-          )}
-        </div>
-
-        <div className="p-5 space-y-5 hover:text-gray-500">
-          <h2 className="font-bold">{title}</h2>
-          <div className="flex items-center gap-2">
-            <Image
-              src={profileImage}
-              width={24}
-              height={24}
-              alt="profile"
-              className="rounded-full"
-              priority={false}
-            />
-            <div className="flex flex-col text-xs text-gray-500">
-              <span>{profileName}</span>
-              <time dateTime={createdAt}>{formattedDate}</time>
+          </CardHeader>
+          <CardContent className="p-4">
+            <h2 className="font-bold text-xl">{title}</h2>
+          </CardContent>
+          <CardFooter className="p-4 pt-0">
+            <div className="flex items-center gap-2">
+              <Image
+                src={profileImage}
+                width={24}
+                height={24}
+                alt="profile"
+                className="rounded-full"
+                priority={false}
+              />
+              <div className="flex flex-col text-xs text-gray-500">
+                <span>{profileName}</span>
+                <time dateTime={createdAt}>{formattedDate}</time>
+              </div>
             </div>
-          </div>
-        </div>
-      </Link>
+          </CardFooter>
+        </Link>
+      </Card>
     </article>
   );
 };
